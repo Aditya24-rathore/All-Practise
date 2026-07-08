16966b6e4e50a7e47de36496146c5c5223f8db51
Aditya24-rathore <ra4402538@gmail.com>
2026-07-08 12:02:26 -0700

    update:

---
+++ Python Practise/string.py
@@ -0,0 +1,24 @@
+s='python'
+print(s.upper())
+s="python PROGRAMMING"
+print(s.lower())
+s='this is python programming'
+print(s.capitalize())
+s='python is easy language'
+print(s.title())
+print(s.swapcase())
+print(s.replace('python','java'))
+
+s="Hello"
+print(s.index('o'))
+
+s="Python Java C++"
+print(s.split())
+s="apple,mango,banana"
+print(s.split(','))
+
+l=['python','java','c++']
+l1=["A","B"]
+print("-".join(l1))
+s=" "
+print(s.join(l))
